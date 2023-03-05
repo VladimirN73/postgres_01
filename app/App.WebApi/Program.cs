47aa6ee302f5cc407ce461527d89f7c9568c5c6a
Vladimir Nalivaika <vladimir.nalivaika@hoffmann-group.com>
2023-03-05 17:56:47 +0100

    add frontend

--- app/App.WebApi/Program.cs
+++ app/App.WebApi/Program.cs
@@ -4,30 +4,54 @@ namespace App.WebApi;
 
 public class Program
 {
+    
+
     public static void Main(string[] args)
     {
+        string corsPolicyName = "CustomRequestOriginPolicy";
+
         var builder = WebApplication.CreateBuilder(args);
+
+        var services = builder.Services;
 
         // Add services to the container.
 
-        builder.Services.AddControllers();
+        services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
 
-        builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApiDbContext>(opt =>
-            opt.UseNpgsql(builder.Configuration.GetConnectionString("db") ?? string.Empty));
+        services.AddCors(options =>
+        {
+            options.AddPolicy(corsPolicyName, policyBuilder =>
+            {
+                policyBuilder.AllowAnyOrigin();
+                policyBuilder.AllowAnyHeader();
+                policyBuilder.AllowAnyMethod();
+            });
+        });
+
+
+        var connectionString = builder.Configuration.GetConnectionString("db") ?? string.Empty;
+        Console.WriteLine($"connectionString: '{connectionString}'");
+        builder.Services
+            .AddEntityFrameworkNpgsql()
+            .AddDbContext<ApiDbContext>(opt => opt.UseNpgsql(connectionString));
         
         var app = builder.Build();
 
         Console.WriteLine($"app.Environment.IsDevelopment()='{app.Environment.IsDevelopment()}'");
+        Console.WriteLine($"app.Environment='{app.Environment}'");
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseCors(corsPolicyName); //https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-7.0
 
         app.UseHttpsRedirection();
 
